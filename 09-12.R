74aa1053b23b6ddea454dd08eef2e929ad4b5968
bmwoodruff <bmwoodruff@gmail.com>
2023-09-12 11:38:13 -0600

    first commit after class Tues Sep 12

---
+++ 09-12.R
@@ -0,0 +1,19 @@
+3+4
+(3+4)^2
+3^2+4^2
+
+f <- function(x){x^2}
+f(1)
+f(2)
+f(3)
+f(3+4) == f(3)+f(4)
+
+g = function(x){x^2}
+#Common convention is to use <- for assigning variables. We'll use = inside functions
+#for locally assigning variables.
+
+x <- 4
+ 
+x < -1
+x<-1  #careful, spacing matters.  Keep spaces between operators and variables. 
+
