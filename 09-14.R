e46d7f9442894aedfd8e109b0ba14f7b184104f2
bmwoodruff <bmwoodruff@gmail.com>
2023-09-14 11:34:53 -0600

    after class Thur Sep 14

--- 09-14.R
+++ 09-14.R
@@ -51,3 +51,28 @@ y <- f(x)
 #y
 plot(x,y, type = "l")
 plot(x,f(x), type = "l")
+
+
+
+#Scatter plots
+mtcars
+?mtcars
+mtcars$wt
+
+par(mar=c(4,4,0.25,0.25))
+x <- mtcars$wt
+y <- mtcars$mpg
+plot(x,y)
+
+
+par(mar=c(4,4,2,0.25))
+x <- mtcars$wt
+y <- mtcars$mpg
+plot(x,y,
+     pch=11,
+     xlab='weight (1000 lbs)',
+     ylab='Miles per US gallon',main='Our 1st Scatter Plot')
+
+#We can also put everything on a single line, as done below, but the above is easier to read.
+plot(x,y,pch=16,xlab='weight (1000 lbs)',ylab='Miles per US gallon',main='Our 1st Scatter Plot')
+
