ddd3ae285a203adc57c8a346f2b02898d77a1efe
bmwoodruff <bmwoodruff@gmail.com>
2023-09-14 10:34:06 -0600

    in class 9/14

---
+++ 09-14.R
@@ -0,0 +1,53 @@
+(-2)^2
+(-1)^2
+(0)^2
+(1)^2
+
+x <- c(-2,-1,0,1,2,3,4,5)
+x
+x^2
+x <- -2:5
+x <- -20:300
+x
+x^2
+
+
+x <- c(-2,-1,0,1,2,3,4,5)
+x
+x <- seq(-2,5)
+x
+x <- seq(-10,10,by = 2)
+x
+ 
+x <- seq(-2,5)
+y <- x^2
+plot(x,y)
+plot(x,y, type = "l")
+
+
+x <- seq(-2,5,0.1)
+y <- x^2
+plot(x,y)
+plot(x,y, type = "l")
+
+f <- function(x){x^2}
+x <-  seq(-2,5,0.1)
+y <- f(x)
+plot(x,y, type = "l")
+plot(x,f(x), type = "l")
+
+
+f <- function(x){
+  ifelse(x < 1, 2*x, 3)
+}
+f(2)
+f(7)
+f(-10)
+f(1)
+f(.999)
+
+x <-  seq(-2,5,0.01)
+y <- f(x)
+#y
+plot(x,y, type = "l")
+plot(x,f(x), type = "l")
