63a4bb74d79913778a3667bf1defa814cd3644ef
bmwoodruff <bmwoodruff@gmail.com>
2023-11-06 11:18:20 -0700

    after class Nov 11-06

---
+++ 11-06.R
@@ -0,0 +1,24 @@
+v <- c(2,2,2,3,3,5,5,5,5,7,8,8,9,9,9)
+sum(v)/length(v)
+mean(v)
+
+
+values <- c(2,3,5,7,8,9)
+frequencies <- c(3,2,4,1,2,3)
+sum(values*frequencies)/sum(frequencies)
+
+
+
+values <- c(2,3,5,7,8,9)
+proportions <- c(3/15,2/15,4/15,1/15,2/15,3/15)
+sum(values*proportions)
+
+values <- c(92,100,70)
+proportions <- c(.70,.20,.10)
+sum(proportions)
+sum(values*proportions)
+
+
+x <- c(0,1,2,3)
+p <- c(1/8,3/8,3/8,1/8)
+sum(x*p)
